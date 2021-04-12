b0b0db5cc4397db2b2df3759a274743e328a49a7
Christopher Evans <christopher.evans@mediamonks.com>
2021-04-12 13:00:49 +0200

    Bootstrap project; Set up item cards

---
+++ src/App.styles.ts
@@ -0,0 +1,3 @@
+import styled from "styled-components";
+
+export const StyledAppWrapper = styled.div``;
---
+++ src/components/Item/Item.tsx
@@ -0,0 +1,24 @@
+import { CartItemType } from "../../App";
+
+import Button from "@material-ui/core/Button";
+
+import { StyledItemWrapper, StyledItemInfo } from "./Item.styles";
+
+type Props = {
+  item: CartItemType;
+  handleAddToCart: (clickedItem: CartItemType) => void;
+};
+
+const Item: React.FC<Props> = ({ item, handleAddToCart }) => (
+  <StyledItemWrapper>
+    <img src={item.image} alt={item.title} />
+    <StyledItemInfo>
+      <p>{item.title}</p>
+      <p>{item.description}</p>
+      <p>${item.price}</p>
+    </StyledItemInfo>
+    <Button onClick={() => handleAddToCart(item)}>Add To Cart</Button>
+  </StyledItemWrapper>
+);
+
+export default Item;
