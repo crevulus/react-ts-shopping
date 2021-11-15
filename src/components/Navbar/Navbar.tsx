ba570b6a536266dc749b785aa5d7277be38383f8
Christopher Evans <christopher.evans@mediamonks.com>
2021-11-15 17:14:02 +0100

    push test to staging

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -2,5 +2,5 @@ import React from "react";
 import { StyledNavbar } from "./Navbar.styles";
 
 export const Navbar = () => {
-  return <StyledNavbar>This is the staging site</StyledNavbar>;
+  return <StyledNavbar>This is the staging site. Push test.</StyledNavbar>;
 };
