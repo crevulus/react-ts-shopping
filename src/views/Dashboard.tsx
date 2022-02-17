392ee50984c8fc9e999b05412278b3cb933d0366
Christopher Evans <christopher.evans@mediamonks.com>
2022-02-17 15:02:38 +0100

    Add plop capabilities

--- src/views/Dashboard.tsx
+++ src/views/Dashboard.tsx
@@ -1,13 +1,12 @@
 import { useContext } from "react";
 import { AuthContext } from "../utils/AuthContext";
 
-export const Dashboard = ({}) => {
+export const Dashboard = () => {
   const { token } = useContext(AuthContext);
 
   return (
     <>
       <h2>Dashboard (Protected)</h2>
-
       <div>Authenticated as {token}</div>
     </>
   );
